044daca6cd31159090f5bf141608896448d2764a
Lucas Eid <lucaseid@protonmail.com>
2021-09-09 14:55:42 -0300

    Update articles.js

--- routes/articles.js
+++ routes/articles.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const article = require('../models/article');
 const router = express.Router();
-const Article = required("./../models/article");
+const Article = require("./../models/article");
 
 router.get('/new', (req, res) =>{
     res.render("articles/new", { article: new Article() })
@@ -16,7 +16,7 @@ router.post('/', async(req, res) =>{
     let article = new Article({
         title: req.body.title,
         description: req.body.description,
-        markdown: req.body.makdown,
+        markdown: req.body.markdown,
     })
         try{ 
             article = await article.save();
