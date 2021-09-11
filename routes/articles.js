8d3a38bc4c948f10cb45f96af9de7ce65ab5c734
Lucas Eid <lucaseid@protonmail.com>
2021-09-11 10:34:52 -0300

    Final

--- routes/articles.js
+++ routes/articles.js
@@ -8,14 +8,22 @@ router.get('/new', (req, res) =>{
     res.render("articles/new", { article: new Article() })
 })
 
+router.get('/edit/:id', async (req, res)=> {
+    const article = await Article.findById(req.params.id)
+    res.render('articles/edit', { article: new Article() })
+}) // Criação da seção de edição
+
 router.get('/:id', async(req, res) => {
     const article = await Article.findById(req.params.id);
     if (article == null) res.redirect('/')
     res.render('articles/show', {article: article})
 })
 //para pedir por banco de dados
-router.post('/', async(req, res) =>{
-    let article = new Article({        
+
+router.post('/', async(req, res, next) => {
+    req.article = new Article()
+    next()
+    /* let article = new Article({        
         title: req.body.title,
         description: req.body.description,
         markdown: req.body.markdown,
@@ -26,8 +34,13 @@ router.post('/', async(req, res) =>{
         } catch(e){
             console.log(e)
             res.redirect("/")
-        }
-})
+        } */
+}, salvar_editar('/new'))
+
+router.put('/:id', async (req, res, next) => {
+    req.article = await Article.findById(req.params.id)
+    next()
+}, salvar_editar('edit'))
 
 router.delete('/:id', async (req, res) => {
     await Article.findByIdAndDelete(req.params.id)
@@ -38,5 +51,21 @@ router.delete('/:id', async (req, res) => {
 Fazer importação dela no server js
 
 */
+
+function salvar_editar(path) {
+    return async (req, res) => {
+        let article = req.article
+        article.title = req.body.title
+        article.description = req.body.description
+        article.markdown = req.body.markdown
+        try { 
+            article = await article.save();
+            res.redirect(`/articles/${article.id}`)
+        } catch(e){
+            console.log(e)
+            res.redirect("/")
+        }
+      }
+    }
 
 module.exports = router;
