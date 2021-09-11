bb975ce21b13b28cf232b883809ded72ca1d0ce8
Lucas Eid <lucaseid@protonmail.com>
2021-09-10 21:08:27 -0300

    mudancas feitas

--- routes/articles.js
+++ routes/articles.js
@@ -1,30 +1,42 @@
 const express = require('express');
 const article = require('../models/article');
 const router = express.Router();
-const Article = required("./../models/article");
+const Article = require("./../models/article");
+
 
 router.get('/new', (req, res) =>{
     res.render("articles/new", { article: new Article() })
 })
 
 router.get('/:id', async(req, res) => {
-    const article = await Article.findByid(req.params.id);
+    const article = await Article.findById(req.params.id);
+    if (article == null) res.redirect('/')
     res.render('articles/show', {article: article})
 })
 //para pedir por banco de dados
 router.post('/', async(req, res) =>{
-    let article = new Article({
+    let article = new Article({        
         title: req.body.title,
         description: req.body.description,
-        markdown: req.body.makdown,
+        markdown: req.body.markdown,
     })
-        try{ 
+        try { 
             article = await article.save();
-            res.redirect(`/article/${id}`)
-        }catch(e){
+            res.redirect(`/articles/${article.id}`)
+        } catch(e){
             console.log(e)
             res.redirect("/")
         }
 })
+
+router.delete('/:id', async (req, res) => {
+    await Article.findByIdAndDelete(req.params.id)
+    res.redirect('/')
+})
+
+/*  Após escrever a função de exclusão instalar o method-override que faz get e delete ao mesmo tempo
+Fazer importação dela no server js
+
+*/
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 
 const port = 700
 const mongoose = require('mongoose');
+
 mongoose.connect('mongodb://localhost/blog',{
     useNewUrlParser:true, 
     useUnifiedTopology: true
@@ -12,6 +14,8 @@ mongoose.connect('mongodb://localhost/blog',{
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false}));
+app.use(methodOverride('_method'))
+
 app.get('/', async(req, res) => {
     const articles = await Article.find().sort({createdAt: 'desc'})
     res.render('articles/index', {articles: articles})
